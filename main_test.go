b1e934c5f935f4bc85a2368f9b4353e01d1648d2
Fei Zhang <fei.zhang@leica-microsystems.com>
2020-10-22 12:24:44 +0800

    add test case

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
 	"testing"
+	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +13,45 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	var (
+		err     error
+		student User
+	)
 
-	DB.Create(&user)
+	if err = DB.Callback().Update().Register("user_update_callback", func(db *gorm.DB) {
+		if db.Statement.Schema == nil {
+			return
+		}
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+		for _, field := range db.Statement.Schema.Fields {
+			if field.Name != "Name" {
+				continue
+			}
+
+			fmt.Printf("----------------- field[%s] changed? %v ------------------\n", field.Name, db.Statement.Changed(field.Name))
+		}
+	}); err != nil {
+		panic(err)
+	}
+
+	if err = DB.Model(&User{}).First(&student).Error; err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			panic(err)
+		}
+
+		student.Name = fmt.Sprintf("name: %d", time.Now().Unix())
+		if err = DB.Save(&student).Error; err != nil {
+			panic(err)
+		}
+	}
+
+	newName := fmt.Sprintf("name: %d", time.Now().Unix())
+	if err = DB.Model(&User{
+		Model: gorm.Model{
+			ID: student.ID,
+		}}).
+		Updates(&User{Name: newName}).
+		Error; err != nil {
+		panic(err)
 	}
 }
