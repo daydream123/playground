0020979266a7ded0b9042392f8fa929a4b5f49bd
Fei Zhang <fei.zhang@leica-microsystems.com>
2020-12-10 15:53:46 +0800

    submit test

--- main_test.go
+++ main_test.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"gorm.io/gorm"
 	"testing"
-	"time"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -13,45 +11,33 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	var (
-		err     error
-		student User
-	)
-
-	if err = DB.Callback().Update().Register("user_update_callback", func(db *gorm.DB) {
+	if err := DB.Callback().Create().Before("gorm:create").Register("user_create_callback", func(db *gorm.DB) {
 		if db.Statement.Schema == nil {
 			return
 		}
 
-		for _, field := range db.Statement.Schema.Fields {
-			if field.Name != "Name" {
-				continue
+		var language Language
+		if err := db.
+			Model(&Language{}).
+			Where(&Language{Code: "1111"}).
+			Take(&language).
+			Error; err != nil {
+			if !errors.Is(err, gorm.ErrRecordNotFound) {
+				// no such column: companies.code,
+				// 即便我指定了查询 `Language`
+				// 我不能指定 db.Session(&gorm.Session{NewDB: true})，因为sqlite不能同时存在2个connect(准确讲是2个writeable conn)
+				panic(err)
 			}
-
-			fmt.Printf("----------------- field[%s] changed? %v ------------------\n", field.Name, db.Statement.Changed(field.Name))
 		}
 	}); err != nil {
 		panic(err)
 	}
 
-	if err = DB.Model(&User{}).First(&student).Error; err != nil {
+	if err := DB.Save(&Company{
+		Name: "hello world",
+	}).Error; err != nil {
 		if !errors.Is(err, gorm.ErrRecordNotFound) {
 			panic(err)
 		}
-
-		student.Name = fmt.Sprintf("name: %d", time.Now().Unix())
-		if err = DB.Save(&student).Error; err != nil {
-			panic(err)
-		}
-	}
-
-	newName := fmt.Sprintf("name: %d", time.Now().Unix())
-	if err = DB.Model(&User{
-		Model: gorm.Model{
-			ID: student.ID,
-		}}).
-		Updates(&User{Name: newName}).
-		Error; err != nil {
-		panic(err)
 	}
 }
